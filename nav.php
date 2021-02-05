a23564c44f0d257d49ad5835ebfba09279759f6b
anadkir <64309347+anadkir@users.noreply.github.com>
2021-02-05 18:18:07 +0200

    Lisätty nav ja footterin layout

--- nav.php
+++ nav.php
@@ -1,1 +1,16 @@
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+    <div class="container">
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Etusivu</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Linkki</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Linkki</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 <?php
